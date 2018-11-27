869a5dea7eed13167e9f7f3131657c42e02d3533
Jack <jack@medsender.com>
2018-11-27 17:02:41 -0500

    Sort of there!

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -8,80 +10,75 @@ import (
 	"os"
 	"sync"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
-type ID int
-
 type Server struct {
-	mutex          sync.Mutex
-	players        []Player
-	count          int
-	submissionChan chan submission
+	mutex   sync.Mutex
+	players []Player
+	count   int
+
+	words []word
+	story string
+
+	gameIsHappening bool
+	submissionChan  chan submission
 }
 
 type Player struct {
-	id          ID
+	ID          int `json:"id"`
 	active      bool
-	messageChan chan string
+	messageChan chan []byte
 }
 
 type submission struct {
-	id ID
+	playerID int
+	WordID   int `json:"id"`
 }
 
-func newServer(cap int) *Server {
+type Update struct {
+	Players []Player `json:"players,omitempty"`
+	Words   []word   `json:"words,omitempty"`
+	Turn    int      `json:"turn"`
+	Story   string   `json:"story"`
+}
+
+type word struct {
+	ID   int    `json:"id"`
+	Word string `json:"word"`
+}
+
+func NewServer(cap int) (*Server, error) {
+
 	s := &Server{}
 	s.players = make([]Player, cap)
 	s.submissionChan = make(chan submission)
-	return s
+
+	// Load words from their file.
+	file, err := os.Open("words.txt")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	id := 0
+	for scanner.Scan() {
+		w := word{
+			ID:   id,
+			Word: string(scanner.Bytes()),
+		}
+		s.words = append(s.words, w)
+		id++
+	}
+
+	return s, nil
 }
 
 func (s *Server) Start() error {
-
-	go func() {
-
-		turn := 0
-
-		for {
-
-			s.mutex.Lock()
-
-			if s.count == 0 {
-				time.Sleep(time.Second)
-				s.mutex.Unlock()
-				continue
-			}
-
-			if !s.players[turn].active {
-				s.mutex.Unlock()
-				turn = (turn + 1) % len(s.players)
-				continue
-			}
-
-			// TODO How to do a timeout on a write?
-			// What if a malicious user hasn't opened
-			// the status endpoint?
-			s.players[turn].messageChan <- "your turn"
-
-			fmt.Println(turn)
-
-			// TODO Wait for the response.
-			time.Sleep(1 * time.Second)
-
-			// Go to the next player.
-			turn = (turn + 1) % len(s.players)
-
-			s.mutex.Unlock()
-
-		}
-
-	}()
-
 	http.HandleFunc("/", s.homeHandler)
-	http.HandleFunc("/status", s.statusHandler)
-	http.HandleFunc("/submit", s.submitHandler)
+	http.HandleFunc("/status", s.websocketHandler)
 	return http.ListenAndServe(":8080", nil)
-
 }
 
 func (s *Server) newPlayer() *Player {
@@ -90,8 +87,8 @@ func (s *Server) newPlayer() *Player {
 	for id := range s.players {
 		if !s.players[id].active {
 			s.players[id].active = true
-			s.players[id].id = ID(id)
-			s.players[id].messageChan = make(chan string)
+			s.players[id].ID = id
+			s.players[id].messageChan = make(chan []byte)
 			s.count++
 			return &s.players[id]
 		}
@@ -124,7 +121,18 @@ func (s *Server) homeHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (s *Server) statusHandler(w http.ResponseWriter, r *http.Request) {
+var upgrader = websocket.Upgrader{} // TODO Make not global.
+
+func (s *Server) websocketHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Upgrade to a websocket connection.
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
 
 	// Create a new Player.
 	p := s.newPlayer()
@@ -134,37 +142,139 @@ func (s *Server) statusHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer s.deletePlayer(p)
 
-	flusher, ok := w.(http.Flusher)
-	if !ok {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println("flushing not supported")
+	// Listen for messages that the game thread want us to send.
+	stop := make(chan struct{})
+	defer func() {
+		stop <- struct{}{}
+	}()
+	go func() {
+		for {
+			select {
+			case <-stop:
+				return
+			case msg := <-p.messageChan:
+				if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+					log.Println(err)
+					return
+				}
+			}
+		}
+	}()
+
+	// If a game isn't happening, start one.
+	s.mutex.Lock()
+	if !s.gameIsHappening {
+		s.gameIsHappening = true
+		go s.startGame()
+	}
+	s.mutex.Unlock()
+
+	// Send the player their ID (and soon init data).
+	idBytes, err := json.Marshal(struct {
+		ID int `json:"id"`
+	}{ID: p.ID})
+	if err != nil {
+		log.Println(err)
 		return
 	}
+	p.messageChan <- idBytes
 
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	// Send the user their id.
-	// TODO Use templated HTML instead.
-	fmt.Fprintf(w, "event: id\r\ndata: %d\r\n\r\n", p.id)
-	flusher.Flush()
-
-	stop := w.(http.CloseNotifier).CloseNotify()
-
+	// Listen for events.
 	for {
-		select {
-		case <-stop:
+
+		// Wait for a message.
+		_, bytes, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err) // TODO Handle disconnect error separately.
 			return
-		case x := <-p.messageChan:
-			fmt.Fprintf(w, "data: %s\r\n\r\n", x)
-			flusher.Flush()
 		}
+
+		// Serialize into a submission and push to the channel.
+		var sub submission
+		if err := json.Unmarshal(bytes, &sub); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Println(err)
+			return
+		}
+		sub.playerID = p.ID
+		s.submissionChan <- sub
+
 	}
 
 }
 
-func (s *Server) submitHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) startGame() {
+
+	turn := 0
+
+	for {
+
+		s.mutex.Lock()
+
+		// Kill the game if everyone left.
+		if s.count == 0 {
+			s.gameIsHappening = false
+			s.mutex.Unlock()
+			return
+		}
+		s.mutex.Unlock()
+
+		// TODO Advance the turn so long as the current player is inactive.
+		for {
+			if s.players[turn].active {
+				break
+			}
+			turn = (turn + 1) % len(s.players)
+		}
+
+		// Get an array of the active players.
+		var activePlayers []Player
+		for _, p := range s.players {
+			if !p.active {
+				continue
+			}
+			activePlayers = append(activePlayers, p)
+		}
+
+		// Broadcast an update.
+		update := Update{
+			Players: activePlayers,
+			Words:   s.words,
+			Turn:    turn,
+			Story:   s.story,
+		}
+		updateBytes, err := json.Marshal(update)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		for _, p := range s.players {
+			if !p.active {
+				continue
+			}
+			p.messageChan <- updateBytes
+		}
+
+		// Expect a response from the current player.
+		ticker := time.NewTicker(5 * time.Second)
+	loop:
+		for {
+			select {
+			case sub := <-s.submissionChan:
+				if sub.playerID != turn {
+					continue
+				}
+				if sub.WordID < 0 || sub.WordID >= len(s.words) {
+					break loop
+				}
+				s.story = s.story + s.words[sub.WordID].Word
+			case <-ticker.C:
+				break loop
+			}
+		}
+
+		turn = (turn + 1) % len(s.players)
+
+	}
 
 }
